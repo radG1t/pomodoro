0f19bdf0707784ad2926aef54a91357cd20127a5
Radin <radinerfanfar@gmail.com>
2023-10-05 11:51:21 +0330

    start again

---
+++ lib/homepage.dart
@@ -0,0 +1,117 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int initialTime = 0;
+  int tl = 0; // time left
+  late Timer timer;
+
+  final TextEditingController _tlController = TextEditingController();
+
+  void _startCountDown() {
+    int inputTime = int.tryParse(_tlController.text) ?? 0;
+
+    if (inputTime > 0) {
+      setState(() {
+        initialTime = inputTime;
+        tl = inputTime;
+      });
+
+      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        if (tl > 0) {
+          setState(() {
+            tl--;
+          });
+        } else {
+          timer.cancel();
+        }
+      });
+    }
+  }
+
+  void _pauseCountDown() {
+    if (timer.isActive) {
+      timer.cancel();
+    }
+  }
+
+  void _resumeCountDown() {
+    if (tl > 0) {
+      timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        if (tl > 0) {
+          setState(() {
+            tl--;
+          });
+        } else {
+          timer.cancel();
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: _tlController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Enter Time (in seconds)',
+              ),
+            ),
+            Text(
+              tl == 0 ? 'DONE' : tl.toString(),
+              style: const TextStyle(color: Colors.black, fontSize: 25),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MaterialButton(
+                  color: Colors.deepPurple,
+                  onPressed: _startCountDown,
+                  child: const Text(
+                    'S T A R T',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                MaterialButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    _pauseCountDown();
+                  },
+                  child: const Text(
+                    'P A U S E',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    _resumeCountDown();
+                  },
+                  child: const Text(
+                    'R E S U M E',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
